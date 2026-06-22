0cb8e8fb72922b6d3d70d09870e89b7addb1590c
Sunyah <emmy.akehurst@gmail.com>
2026-06-22 22:41:34 +0200

    Create troisieme.js
    troisieme mumuse

---
+++ samples/experimentation/troisieme.js
@@ -0,0 +1,26 @@
+setcpm(30)
+
+$: sound("bd!4").bank("RolandTR909").lpf(slider(500,500,2000)).color("white").gain(0.5)
+
+$: note("c4 [f4 g4] -@4")
+  .sound("triangle")
+  .lpf(500)
+  .room(1)
+  .roomsize(4)
+  ._scope()
+  .color("white")
+
+$: note("[A4 B4] <C5 E5>")
+  .sound("triangle")
+  .slow(4) //ralenti le changement de note 
+  .room(1)
+  .roomsize(4)
+  .delay(0.3)
+  .delayfeedback(0.8)
+  .pan(sine.slow(8)) // effet orbitale, sine permet de faire monter la note ou la faire descendre
+  .lpf(perlin.range(500, 2500))
+  .gain(0.5)
+  ._scope()
+  .color("white")
+
+$: note("<[c5 c5 d5 [e5 f5]]>").s("wt_digital_basique").gain(slider(0.305,0,1)).delay(slider(0.815,0,1))
