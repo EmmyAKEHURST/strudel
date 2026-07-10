29e927f43a9eb1517e912ae1620f47899c868402
Emmy AKEHURST <emmy.akehurst@gmail.com>
2026-07-10 08:46:30 +0200

    Add joyfulafternoon.js to experimentation folder

--- samples/experimentation/joyfulafternoon.js
+++ samples/experimentation/joyfulafternoon.js
@@ -6,12 +6,10 @@ const triangle = note("c e g b").s("triangle").scale("<C:major C:minor >")
   .delayfeedback(0.7)
   .pan(sine.slow(2))
   .gain(0.8)
-  ._punchcard()
 
 const bass = sound("bd*8").cutoff(sine.range(200, 2000)
   .slow(4))
   .decay(0.1)
-  ._spectrum()
 
 const melody = note("<[c3,e3,g3] [a2,c3,e3] [f2,a2,c3] [g2,b2,d3]>*2").s("gm_recorder!2")
   .hpf(sine.range(200,1000))
@@ -20,14 +18,13 @@ const melody = note("<[c3,e3,g3] [a2,c3,e3] [f2,a2,c3] [g2,b2,d3]>*2").s("gm_rec
 
 const notes = note("<[c4 e4 g4 b4] [a3 c4 e4 g4] [f3 a3 c4 e4] [g3 b3 d4 f4]>".fast(2))
   .s("triangle")
-  .delay(0.4).delaytime(0.375).delayfeedback(0.5)
+  .delay(0.4).delaytime(0.4).delayfeedback(0.5)
   .room(0.6).roomsize(0.4)
   .pan(sine.slow(3))
-  .gain(0.65)
+  .gain(0.6)
   .shape(0.2)
-  .spiral({ steady: .96 }).color("magenta")
 
 // PLAY WITH triangle, bass, melody or notes
 // made by Sunyah as a first full experimentation
 
-stack()
+stack(triangle, bass, melody, notes).hpf(slider(0,0,1000))
