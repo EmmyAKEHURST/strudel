bd67fc16da7cc02b18db30849c0cd4c9508aeb54
Emmy AKEHURST <emmy.akehurst@gmail.com>
2026-08-03 15:11:02 +0200

    Create glorpiiswatching_wip.js

---
+++ samples/glorpiiswatching_wip.js
@@ -0,0 +1,111 @@
+setcpm(30)
+
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠄⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⡟⠇⠀⠀⠀⠀⢻⡄⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡇⠀⠀⠀⠀⡸⣞⡇⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠃⠀⠀⠀⢀⣧⢿⣽⡀⠀⠀
+// ⠀⢴⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠀⠀⠀⠀⣼⣞⡿⡞⡁⠀⠀
+// ⠀⠀⠈⠓⢤⡀⠀⠀⠀⡀⠀⠀⠀⠀⠀⠀⠀⣾⠀⠀⠀⣰⣟⢾⣽⢃⡿⠀⠀
+// ⠀⠀⠀⠀⠀⠙⢆⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣠⢤⣶⡻⣞⣿⣺⠇⣽⢣⠀
+// ⢠⣄⡀⠀⠀⠀⠀⠙⢦⡀⠀⠀⠀⠀⣀⣠⣤⣿⣽⣻⢾⣽⣷⣾⣽⠃⢆⡇⠀
+// ⠈⢻⣿⣶⣄⡀⠀⠀⠀⣉⣳⣴⢶⣞⡿⣽⣞⡷⣯⢿⡽⣞⣿⠟⠋⠁⠈⠀⠀
+// ⠀⠀⢻⣿⣿⣿⣿⣶⣾⣿⡽⣯⣟⡾⣽⡷⣯⣟⡽⡾⣽⡯⠁⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠉⢞⣿⣿⢯⡿⣿⣯⣟⣷⣯⢿⣳⣟⡷⣽⣼⣻⣽⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⢾⣿⣿⣯⣽⣾⣿⣾⣗⡿⣯⡷⣯⣟⡷⣞⣼⣿⣀⠀⠀⠀⠀⢀⣠⠀
+// ⠀⠀⢀⣼⠛⠏⠉⠉⠽⢟⢿⣿⣿⣿⣿⣷⣻⢾⡽⣞⡷⠄⡹⣶⠟⣻⢿⡃⠀
+// ⠀⠀⣸⣯⠇⠀⠀⠀⠀⠀⠁⣽⣿⣿⣿⣷⣯⣿⣽⣛⡦⠀⠀⢩⠣⣹⢯⡇⠀
+// ⠀⠀⢈⣿⡃⠁⠀⠀⠀⢀⣬⣾⣟⢿⣻⣿⣿⣟⡾⣽⡳⠄⠎⢳⢈⢯⣟⡆⠀
+// ⠀⠀⣸⣷⣷⣶⣳⣶⣺⣿⣿⣷⢯⣟⣿⣿⣳⢯⠛⠅⠃⠀⠀⣴⢪⡿⣬⠂⠀
+// ⠀⠀⢺⡽⣾⡽⣯⣟⣿⡿⣯⣿⣿⣾⢿⣿⠳⢏⣈⢠⠀⠀⣰⢿⡞⣜⣉⠀⠀
+// ⠀⠀⠘⣽⣳⣟⣳⣟⣾⣽⣿⣿⣿⣿⣿⣦⣜⡻⡽⠆⠃⣴⡟⣯⠗⡣⣭⠀⠀
+// ⠀⠀⠀⠹⣿⣾⣿⣯⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣾⠧⠀⠹⠾⡵⡞⠹⢠⠀⠀
+// ⠀⠀⠀⠐⠹⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢯⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⣀⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢯⣏⡰⣶⣵⣢⢄⢠⡀⡄⠀⠀
+// ⠀⠀⡀⠔⠉⠀⠀⢽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣔⣻⢷⣯⡽⣚⣷⣻⡼⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡟⣽⣞⣷⣻⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// Glorpi is watching
+// TombeauLatenT
+
+// -------------------- LES CONSTANTES --------------------
+
+// Voici le gros vvrrrrrrrmmmmm derrière
+const back = note("<c2 g#1 f1 g#1>/2").sound("supersaw")
+  .seg(16)
+  .lpf(200)
+  .gain(1)
+  .distort("2:.5")
+  .room(1)
+  .cutoff(slider(300,300,3000)) //a augmenter lors du drop "wow"
+  .postgain(slider(1,0.1,1))
+  ._pianoroll()
+
+// vais-je mourir ?
+const bipbip = stack(
+  note("<c4!4 f3!2 g#3!2> -!7")
+  .delay(.5)
+  .room(1),
+  
+  note("- - - c6, - - - c5").sound("wt_digital_basique")
+  .room(1)
+  .delay(.2)
+  .sometimes(ply("4"))
+  .postgain(.3)
+)
+
+// ça fait boom
+const boom = sound("bd!4").bank("rolandtr808")
+  .gain(3)
+  ._pianoroll()
+
+// hithat qui part quasi jamais en couille mais quand même
+const hithat = sound("compurhythm1000_hh!8")
+  .decay(.8)
+  .gain(.15)
+  .almostNever(ply("2 | 4"))
+  .lpf(2000)
+  ._pianoroll()
+
+
+// poudre de perlinpinping
+const lesnotes = note("<c2 g#1 f1 g1>/4").sound("wt_digital_basique")
+  .trans(36)
+  .struct("x - x - - x - x").fast(2)
+  .room(1)
+  .sometimes(trans(12))
+  .delay(.5)
+  .postgain(.65)
+  ._pianoroll()
+
+// -------------------- ASSEMBLAGE --------------------
+
+const un = back
+const unbip = stack(back, bipbip)
+const deux = stack(unbip, boom)
+const trois = stack(deux, hithat)
+const wow = stack(trois, lesnotes)
+
+// -------------------- MUSIQUE FINALE !! --------------------
+
+// ⠀⠀⠀⠀⠀⠀⠀⢀⡠⠐⠲⣖⢒⠄⡀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⡀⠀⡤⣴⢫⠖⠁⠀⠁⠉⠁⠈⠄⠀⠀⠀⠀⠀⠀
+// ⣴⣎⠹⠽⠉⠹⣷⣏⣸⣀⣀⡀⠲⢤⡄⢸⣤⡀⠀⠀⠀⠀
+// ⠫⣛⠳⢦⣀⢴⡢⠀⠉⠛⣻⠿⠟⠶⠾⡟⠋⢹⣖⡦⡀⠀
+// ⠀⠀⠙⣖⡤⣍⣓⡂⠀⠀⠷⠃⠀⠀⠸⣾⣀⣤⣬⡱⢞⡆
+// ⠀⠀⠀⠈⠛⠿⣛⠯⠿⠶⣶⣦⠤⡤⡄⢤⣤⡵⠒⠒⠋⠀
+// ⠀⠀⠀⠀⠀⠀⣠⠋⠁⠒⠒⢶⠀⠐⠒⠊⠁⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⢀⠞⠁⠀⠀⠀⠀⠸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠐⠁⠀⠀⠀⠀⠀⠀⠀⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⡠⠊⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢡⠀⠀⠀⠀⠀⠀⠀⠀⠀
+// ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⠀⠀⠀⠀⠀⠀⠀⠀⠀
+
+// temps en cycle, variable assignée
+
+arrange(
+  [8, back],
+  [16, unbip],
+  [8, deux],
+  [8, trois],
+  [64, wow],
+)
